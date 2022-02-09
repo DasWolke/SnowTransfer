d7e2201b15a2f10ffab64fc12a607af12c675f37
Avocado <43632131+Awoocado@users.noreply.github.com>
2022-02-09 10:54:16 -0500

    adding guild scheduled event methods

--- src/SnowTransfer.ts
+++ src/SnowTransfer.ts
@@ -5,6 +5,7 @@ import UserMethods from "./methods/Users";
 import GuildAssetsMethods from "./methods/GuildAssets";
 import WebhookMethods from "./methods/Webhooks";
 import GuildMethods from "./methods/Guilds";
+import GuildScheduledEventMethods from "./methods/GuildScheduledEvent";
 import GuildTemplateMethods from "./methods/GuildTemplate";
 import InteractionMethods from "./methods/Interactions";
 import InviteMethods from "./methods/Invites";
@@ -59,6 +60,7 @@ class SnowTransfer {
 		this.guildAssets = new GuildAssetsMethods(this.requestHandler);
 		this.webhook = new WebhookMethods(this.requestHandler, this.options.disableEveryone);
 		this.guild = new GuildMethods(this.requestHandler);
+		this.guildscheduledevent = new GuildScheduledEventMethods(this.requestHandler);
 		this.guildTemplate = new GuildTemplateMethods(this.requestHandler);
 		this.interaction = new InteractionMethods(this.requestHandler, this.webhook);
 		this.invite = new InviteMethods(this.requestHandler);
